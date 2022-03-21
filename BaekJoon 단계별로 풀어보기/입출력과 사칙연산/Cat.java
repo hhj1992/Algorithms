29386e2cd891b0811576076f4c8fbc8e1e983b81
Aries <92412240+hhj1992@users.noreply.github.com>
2022-03-21 19:24:08 +0900

    Cat.java

--- BaekJoon 단계별로 풀어보기/입출력과 사칙연산/Cat.java
+++ BaekJoon 단계별로 풀어보기/입출력과 사칙연산/Cat.java
@@ -1,12 +1,10 @@
 public class Cat {
-
     public static void main(String[] args) {
 
-        System.out.println("\\   /\\");
+        System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
         System.out.println(" \\(__)|");
 
     }
-
 }
