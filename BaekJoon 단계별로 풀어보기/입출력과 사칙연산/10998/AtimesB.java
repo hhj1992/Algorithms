84bb27f1337e830df594e09c07549ab0195560d2
HwangHyeonJi <hhj0323@naver.com>
2022-06-10 03:35:57 +0900

    times

---
+++ BaekJoon 단계별로 풀어보기/입출력과 사칙연산/10998/AtimesB.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AtimesB {
+
+    public static void main(Stirng[] args){
+
+       Scanner sc = new Scanner(System.in);
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+
+       System.out.println(A * B);
+
+        in.close();
+    }
+
+    
+}
