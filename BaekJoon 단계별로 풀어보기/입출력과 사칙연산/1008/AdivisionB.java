43e8485a91a93020439a776ab99ba8a57cb42993
HwangHyeonJi <hhj0323@naver.com>
2022-06-15 00:45:23 +0900

    'division'

---
+++ BaekJoon 단계별로 풀어보기/입출력과 사칙연산/1008/AdivisionB.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AdivisionB{
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		double A = in.nextDouble();
+		double B = in.nextDouble();
+		
+		System.out.println(A / B);
+		
+		in.close();
+
+	
+	//float 플롯 = in.nextFloat();으로 했을때 틀렸습니다.
+	
+	}
+}
